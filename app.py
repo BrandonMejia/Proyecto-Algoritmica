c4d671f90732053d8a5bb49b06251ea0fe1c8ea1
JavierPortella <54995554+JavierPortella@users.noreply.github.com>
2019-11-28 21:38:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask , render_template, request, redirect, url_for, 
-
-flash
+from flask import Flask , render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -10,3 +8,26 @@ app.config['MYSQL_USER'] ='root'
 app.config['MYSQL_PASSWORD'] =''
 app.config['MYSQL_DB'] ='flaskcontacts'
 mysql = MySQL(app)
+
+#settings
+app.secret_key = 'mysecretkey'
+
+@app.route('/')
+def Index():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM contacts')
+    data = cur.fetchall()
+    return render_template('index.html', contacts= data)
+
+@app.route('/add_contact' , methods=['POST'])
+def add_contact():
+    if request.method == 'POST' :
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO contacts(FULLNAME,TELEFONO,EMAIL) VALUES(%s, %s, %s)',
+        (fullname, phone, email))
+        mysql.connection.commit()
+        flash('Contacto agregado satisfactoriamente')
+        return redirect(url_for('Index'))
